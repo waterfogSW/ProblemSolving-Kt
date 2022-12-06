d417fad570f17cc6b6148646ac786c26ccca838d
waterfog9580 <waterfog9580@gmail.com>
2022-12-06 12:37:20 +0900

    :sparkles: 이진변환 반복하기

---
+++ src/main/kotlin/programmers/Solution_70129.kt
@@ -0,0 +1,17 @@
+package programmers
+
+class Solution_70129 {
+    fun solution(s: String): IntArray {
+        var loopCount = 0
+        var removedZero = 0
+        var temp = s
+
+        while (temp != "1") {
+            removedZero += temp.replace("1", "").count()
+            temp = Integer.toBinaryString(temp.replace("0", "").count())
+            loopCount++
+        }
+
+        return intArrayOf(loopCount, removedZero)
+    }
+}
